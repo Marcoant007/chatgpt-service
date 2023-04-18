40174b9bffda8c1347f81ccca9af2a15f72a3477
Marcoant007 <marcoantnovo@gmail.com>
2023-04-18 01:22:00 -0300

    init project and create usecases

---
+++ internal/domain/gateway/chat.go
@@ -0,0 +1,13 @@
+package gateway
+
+import (
+	"context"
+
+	"github.com/marcoant007/chatgptservice/internal/domain/entity"
+)
+
+type ChatGateway interface {
+	CreateChat(ctx context.Context, chat *entity.Chat) error
+	FindChatId(ctx context.Context, chatID string) (*entity.Chat, error)
+	SaveChat(ctx context.Context, chat *entity.Chat) error
+}
